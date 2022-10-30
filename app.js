3f004d18d15149665f3a00acdaa42991eef4075d
VladGridin <vldgrdn@gmail.com>
2022-10-30 13:28:49 +0200

    g

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ tg.MainButton.color = "#2cab37";
 let item = "";
 
 
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn1m = document.getElementById("1-");
-let btn1p = document.getElementById("1+");
-
 var qw = "1"
 var we = "1"
 var er = "1"
@@ -34,18 +27,6 @@ function btn1(){
 	}
 };
 
-// btn1.addEventListener("click", function(){
-// 	document.getElementById("txt1").innerHTML = `<p class="text">${qw*10}л</p>`;
-// 	document.getElementById("btn_1").innerHTML='<input type="button" onclick="Minus()" value="-" class="btn">' + '<input type="button" onclick="Plus()" value="+" class="btn">';
-// 	if (tg.MainButton.isVisible) {
-// 		tg.MainButton.hide();
-// 	}
-// 	else {
-// 		tg.MainButton.setText("Вы выбрали товар 1!");
-// 		item = `Газ ${qw*10}л`;
-// 		tg.MainButton.show();
-// 	}
-// });
 
 function Minus(){
 	if (qw> 1) {
