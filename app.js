c0be16156783e506d21fb3095ac60c2c0c6abaef
VladGridin <vldgrdn@gmail.com>
2022-10-30 13:41:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var ty = "1"
 
 function btn1(){
 	document.getElementById("txt1").innerHTML = `<p class="text">${qw*10}л</p>`;
-	document.getElementById("btn_1").innerHTML='<input type="button" onclick="Minus()" value="-" class="btn">' + '<input type="button" onclick="Plus()" value="+" class="btn">';
+	document.getElementById("btn_1").innerHTML='<input type="button" onclick="Minus1()" value="-" class="btn">' + '<input type="button" onclick="Plus1()" value="+" class="btn">';
 		if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
@@ -28,7 +28,7 @@ function btn1(){
 };
 
 
-function Minus(){
+function Minus1(){
 	if (qw> 1) {
 		qw--
 		document.getElementById("txt1").innerHTML = `<p class="text">${qw*10}л</p>`;
@@ -49,7 +49,7 @@ function Minus(){
 		}
 	}
 };
-function Plus(){
+function Plus1(){
 	if (qw< 5){
 		qw++
 		document.getElementById("txt1").innerHTML = `<p class="text">${qw*10}л</p>`;
@@ -60,6 +60,57 @@ function Plus(){
 	else {
 			tg.MainButton.setText(`Газ ${qw*10}л`);
 			item = `Газ ${qw*10}л`;
+			tg.MainButton.show();
+
+		
+	}
+};
+
+function btn2(){
+	document.getElementById("txt2").innerHTML = `<p class="text">${we*10}л</p>`;
+	document.getElementById("btn_2").innerHTML='<input type="button" onclick="Minus2()" value="-" class="btn">' + '<input type="button" onclick="Plus2()" value="+" class="btn">';
+		if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText(`Газ ${we*10}л`);
+		item = `Газ ${we*10}л`;
+		tg.MainButton.show();
+	}
+};
+
+function Minus2(){
+	if (we> 1) {
+		we--
+		document.getElementById("txt2").innerHTML = `<p class="text">${we*10}л</p>`;
+		tg.MainButton.setText(`Газ ${we*10}л`);
+		item = `Газ ${we*10}л`;
+		tg.MainButton.show();
+	}
+	else {
+		document.getElementById("btn_2").innerHTML='<input type="button" onclick="btn2()" value="Buy" class="btn">';
+		document.getElementById("txt2").innerHTML ="";
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText(`Газ ${we*10}л`);
+			item = `Газ ${we*10}л`;
+			tg.MainButton.show();
+		}
+	}
+};
+function Plus2(){
+	if (we< 5){
+		we++
+		document.getElementById("txt2").innerHTML = `<p class="text">${we*10}л</p>`;
+		tg.MainButton.setText(`Газ ${we*10}л`);
+		item = `Газ ${we*10}л`;
+		tg.MainButton.show();
+	}
+	else {
+			tg.MainButton.setText(`Газ ${we*10}л`);
+			item = `Газ ${we*10}л`;
 			tg.MainButton.show();
 
 		
